4c4274e67e769c4d65ee76867bc12bea3830c556
Seegan07 <seeganpaul47.sp@gamil.com>
2022-09-03 13:09:03 +0530

    project

---
+++ src/com/dayone/org/Javadayone.java
@@ -0,0 +1,38 @@
+package com.dayone.org;
+
+public class Javadayone {
+
+	private void stuName() {
+		
+		System.out.println("vikram");
+		
+	}
+	
+	
+	public static void main(String[] args) {
+		
+	//class name
+	  Javadayone a = new Javadayone();
+	  a.stuName();
+		
+		
+		
+		
+	}
+	
+
+	
+	
+	
+	
+	
+		
+	
+	
+	
+	
+	
+	
+	
+	
+}
